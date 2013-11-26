4c6175a27500da0cec9007b6dd39dc97f50a7903
David Clarke <terrorhawks@gmail.com>
2013-11-26 19:19:36 +1100

    added requires and bumped version

--- lib/wilbertils.rb
+++ lib/wilbertils.rb
@@ -1,5 +1,7 @@
 require "wilbertils/version"
 require "wilbertils/search/locality_search"
+require "wilbertils/toggle"
+require "wilbertils/message_receiver_stack"
 module Wilbertils
   # Your code goes here...
 end
--- lib/wilbertils/version.rb
+++ lib/wilbertils/version.rb
@@ -1,3 +1,3 @@
 module Wilbertils
-  VERSION = "0.0.9"
+  VERSION = "0.1.0"
 end
