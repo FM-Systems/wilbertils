e2ee1debcc1352d33e83b6cf6841f82c62d1fc62
Trevor Nuridin <trevor@myfreight.com.au>
2017-04-12 16:53:03 +1000

    handle different ways of searching for localities based on country. WIP

--- lib/wilbertils/search/locality_search.rb
+++ lib/wilbertils/search/locality_search.rb
@@ -2,24 +2,56 @@ module Wilbertils; module Search
   module LocalitySearch
 
     module ClassMethods
-      def match(locality_name, orig_postcode, threshold=2, locality_search = Wilbertils::Search::LocalitySearcher.new)
-        postcode = "%04d" % orig_postcode.to_i
+      def match(params)
+        case params[:country]
+          when 'Australia'
+            search_locality_postcode_region(params)
+          when 'New Zealand'
+            search_sublocality_locality_postcode(params)
+          else
+            search_locality_postcode_region(params)
+        end
+      end
 
-        l = Locality.find_by_locality_and_postcode locality_name, postcode
-        return l if l
+      def search_locality_postcode_region(params, threshold=2, locality_search = Wilbertils::Search::LocalitySearcher.new)
+        postcode = "%04d" % params[:postcode].to_i
 
-        unless postcode=~/^\d{4}$/ && locality_name && locality_name.length > 0
-          Rails.logger.info("postcode or locality not valid '#{orig_postcode}' or '#{locality_name}'")
+        l = Locality.where(locality: params[:locality], postcode: postcode, region: params[:region], country: params[:country]).first
+        return l if (l || params[:fuzzy] == false)
+
+        unless postcode=~/^\d{4}$/ && params[:locality] && params[:locality].length > 0
+          Rails.logger.info("postcode or locality not valid '#{params[:postcode]}' or '#{params[:locality]}'")
           return nil
         end
 
-        search = locality_search.match_closest_location(locality_name, postcode)
+        search = locality_search.match_closest_location(params[:locality], postcode)
         if (search.max_score < threshold)
-          search = locality_search.match_closest_location(locality_search.find_best_word(locality_name), postcode)
+          search = locality_search.match_closest_location(locality_search.find_best_word(params[:locality]), postcode)
         end
         result = search.results.first
         Locality.find(result[:id]) unless search.max_score < threshold || result[:id].nil?
       end
+
+
+      def search_sublocality_locality_postcode(params, threshold=2, locality_search = Wilbertils::Search::LocalitySearcher.new)
+        postcode = "%04d" % params[:postcode].to_i
+
+        l = Locality.where(sublocality: params[:sublocality], locality: params[:locality], postcode: postcode, country: params[:country]).first
+        return l if (l || params[:fuzzy] == false)
+
+        unless postcode=~/^\d{4}$/ && params[:locality] && params[:locality].length > 0
+          Rails.logger.info("postcode or locality not valid '#{params[:postcode]}' or '#{params[:locality]}'")
+          return nil
+        end
+
+        search = locality_search.match_closest_location(params[:locality], postcode)
+        if (search.max_score < threshold)
+          search = locality_search.match_closest_location(locality_search.find_best_word(params[:locality]), postcode)
+        end
+        result = search.results.first
+        Locality.find(result[:id]) unless search.max_score < threshold || result[:id].nil?
+      end
+
     end
 
     def self.included(base)
