f45926c4becf351e8e30c2535f06d4243e46499d
Trevor Nuridin <trevor@myfreight.com.au>
2017-12-20 11:49:56 +1100

    remove country specific locality matching

--- lib/wilbertils/search/locality_search.rb
+++ lib/wilbertils/search/locality_search.rb
@@ -2,21 +2,10 @@ module Wilbertils; module Search
   module LocalitySearch
 
     module ClassMethods
-      def match(params)
-        case params[:country].upcase
-          when 'AUSTRALIA'
-            search_locality_postcode_region(params)
-          when 'NEW ZEALAND'
-            search_sublocality_locality_postcode(params)
-          else
-            search_locality_postcode_region(params)
-        end
-      end
-
-      def search_locality_postcode_region(params, threshold=2, locality_search = Wilbertils::Search::LocalitySearcher.new)
+      def match(params, threshold=2, locality_search = Wilbertils::Search::LocalitySearcher.new)
         postcode = "%04d" % params[:postcode].to_i
 
-        l = Locality.where(locality: params[:locality], postcode: postcode, region: params[:region], country: params[:country]).first
+        l = Locality.where(sublocality: params[:sublocality], locality: params[:locality], postcode: postcode, region: params[:region], country: params[:country]).first
         return l if (l || !params[:fuzzy])
 
         unless postcode=~/^\d{4}$/ && params[:locality] && params[:locality].length > 0
@@ -31,27 +20,6 @@ module Wilbertils; module Search
         result = search.results.first
         Locality.find(result[:id]) unless search.max_score < threshold || result[:id].nil?
       end
-
-
-      def search_sublocality_locality_postcode(params, threshold=2, locality_search = Wilbertils::Search::LocalitySearcher.new)
-        postcode = "%04d" % params[:postcode].to_i
-
-        l = Locality.where(sublocality: params[:sublocality], locality: params[:locality], postcode: postcode, country: params[:country]).first
-        return l if (l || !params[:fuzzy])
-
-        unless postcode=~/^\d{4}$/ && params[:locality] && params[:locality].length > 0
-          Rails.logger.info("postcode or locality not valid '#{params[:postcode]}' or '#{params[:locality]}'")
-          return nil
-        end
-
-        search = locality_search.match_closest_location(params[:locality], postcode)
-        if (search.max_score < threshold)
-          search = locality_search.match_closest_location(locality_search.find_best_word(params[:locality]), postcode)
-        end
-        result = search.results.first
-        Locality.find(result[:id]) unless search.max_score < threshold || result[:id].nil?
-      end
-
     end
 
     def self.included(base)
@@ -60,6 +28,7 @@ module Wilbertils; module Search
 
   end
 
+  # fuzzy matching has caused issues with wrong matches, its value is questionable
   class LocalitySearcher
 
     def initialize(stop_words =%w(mount sands flat beach brook west east north south island river saint))
