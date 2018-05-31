9e256fdf6860443b3b3c68d244aee39014b20f0b
Subash KC <subashkc1@gmail.com>
2018-05-31 10:29:45 +1000

    key now gets passed in instead of being served from env file

--- lib/wilbertils/encrypt_decrypt.rb
+++ lib/wilbertils/encrypt_decrypt.rb
@@ -1,22 +1,19 @@
-require 'dotenv/load'
 module Wilbertils
   class EncryptDecrypt
 
-    KEY = ENV['URL_ENCRYPT_DECYRPT_KEY']
-
     class << self
 
-      def encrypt plain_value
+      def encrypt plain_value, key
         cipher = OpenSSL::Cipher.new('DES-EDE3-CBC').encrypt
-        cipher.key = Digest::SHA1.hexdigest KEY
+        cipher.key = Digest::SHA1.hexdigest key
         s = cipher.update(plain_value) + cipher.final
 
         s.unpack('H*')[0].upcase
       end
 
-      def decrypt encoded_value
+      def decrypt encoded_value, key
         cipher = OpenSSL::Cipher.new('DES-EDE3-CBC').decrypt
-        cipher.key = Digest::SHA1.hexdigest KEY
+        cipher.key = Digest::SHA1.hexdigest key
         s = [encoded_value].pack("H*").unpack("C*").pack("c*")
 
         cipher.update(s) + cipher.final
--- wilbertils.gemspec
+++ wilbertils.gemspec
@@ -21,7 +21,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rspec"
   spec.add_development_dependency "rake"
-  spec.add_development_dependency "dotenv"
   spec.add_runtime_dependency "log4r", "~> 1.1.10"
   spec.add_runtime_dependency "statsd-ruby"
   spec.add_runtime_dependency 'aws-sdk', '3.0.1'
