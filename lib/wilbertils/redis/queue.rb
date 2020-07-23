14bb5174711008f0545abd6a11d9ecd16f97a070
Subash KC <subashkc1@gmail.com>
2020-07-23 15:37:47 +1000

    reusing the existing redis connection didn't work with WF, putting the changes back

--- lib/wilbertils/redis/queue.rb
+++ lib/wilbertils/redis/queue.rb
@@ -3,15 +3,18 @@ require 'redis-queue'
 module Wilbertils; module Redis
   module Queue
     extend self
+    extend Wilbertils::Redis::Redis
     
     @queues = {}
 
-    def queue queue_name
-      get_queue queue_name
+    def queue config, queue_name
+      redis = client config # establish connection to redis server
+      get_queue(queue_name, redis)
     end
     
-    def send_message queue_name, message
-      get_queue(queue_name).push(message_body(message, queue_name))
+    def send_message config, queue_name, message
+      redis = client config # establish connection to redis server
+      get_queue(queue_name, redis).push(message_body(message, queue_name))
     end
     
     private
@@ -21,8 +24,8 @@ module Wilbertils; module Redis
       json_message.merge!(queue_name: queue_name, message_first_received_time: Time.now).to_json
     end
     
-    def get_queue queue_name
-      @queues[queue_name] = @queues[queue_name].nil? ? ::Redis::Queue.new(queue_name, "#{queue_name}_processing", :redis => $redis) : @queues[queue_name]
+    def get_queue queue_name, redis
+      @queues[queue_name] = @queues[queue_name].nil? ? ::Redis::Queue.new(queue_name, "#{queue_name}_processing", :redis => redis) : @queues[queue_name]
     end
 
   end
