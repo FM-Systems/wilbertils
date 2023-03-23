27aa6ae042770f8f06242f97f23f767ebdee9d1d
Sam <smadams00@gmail.com>
2023-03-23 15:13:49 +1100

    Rails support for logging and error handling

--- lib/wilbertils/logger.rb
+++ lib/wilbertils/logger.rb
@@ -1,14 +1,14 @@
 module Wilbertils
   class Logging
     class << self
-      def logger(config)
-        @logger ||= if config.environment == 'development'
-          l = ActiveSupport::Logger.new("log/#{config.environment}.log")
+      def logger(config = nil)
+        @logger ||= if ENV["ENVIRONMENT_NAME"] == 'development'
+          l = ActiveSupport::Logger.new("log/#{ENV["ENVIRONMENT_NAME"]}.log")
           l.formatter = proc { |sev, date, _, msg| "#{date.strftime('%Y-%m-%d %H:%M:%S')} #{sev}: #{msg}\n" }
           l
         else
           # This logger is just a wrapper around Logger that sets a NewRelic JSON decorator
-          NewRelic::Agent::Logging::DecoratingLogger.new("log/#{config.environment}.log")
+          NewRelic::Agent::Logging::DecoratingLogger.new("log/#{ENV["ENVIRONMENT_NAME"]}.log")
         end
       end
     end
--- lib/wilbertils/version.rb
+++ lib/wilbertils/version.rb
@@ -1,3 +1,3 @@
 module Wilbertils
-  VERSION = "1.6.1"
+  VERSION = "1.6.2"
 end
