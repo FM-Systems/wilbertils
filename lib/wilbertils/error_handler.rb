0fd133a6d7bcbdfcb7fa9fc7d62e544a21447cfa
Sam <smadams00@gmail.com>
2023-04-03 15:24:30 +1000

    Record invalid error added to ErrorHandler
    For sites and site services errors are now allowed to raise up to the error handler but need a specific body to be returned to show in the front end.

--- lib/wilbertils/error_handler.rb
+++ lib/wilbertils/error_handler.rb
@@ -11,11 +11,18 @@ module Wilbertils
       rescue_from(ActionController::InvalidAuthenticityToken)  { |e| rescue_error(e, error_code: :unauthorized,   status: :unauthorized) }
       rescue_from(ActiveRecord::StaleObjectError)              { |e| rescue_error(e, error_code: :stale,          status: :internal_server_error) }
       rescue_from(ActiveRecord::RecordNotFound)                { |e| rescue_error(e, error_code: :missing_record, status: :not_found) }
+      rescue_from(ActiveRecord::RecordInvalid)                 { |e| rescue_invalid_record(e) }
     end
 
     def rescue_error(error, **options)
       Wilbertils::ErrorHandler.rescue_error(error, **options)
       render_error(error, options[:error_code], options[:status]) unless options[:render_error] == false
+    end
+
+    def rescue_invalid_record error, **options
+      rescue_error(error, **options.merge(render_error: false))
+      return unless defined?(render)
+      render json: { errors: error.record.errors }, status: options[:status] || :bad_request
     end
 
     def render_error error, code, status
