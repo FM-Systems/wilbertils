99873c9d623d8ee07f43981683cd909a107ae335
Viraneath <viraneath2010@gmail.com>
2025-03-20 09:27:54 +1100

    Fix Oauth2 token expiry issue.
    Some carriers don't have an expires_in value stored in Redis, so they use the default of 3600 seconds.

--- lib/wilbertils/authorization/oauth2.rb
+++ lib/wilbertils/authorization/oauth2.rb
@@ -154,7 +154,7 @@ module Wilbertils::Authorization
     def expired? token
       # default of 1 hr set, sometimes medipt send back nil and need to handle it
       # Added 60s leeway to ensure token is still valid when it reaches the client endpoint
-      !!token && ( ( Time.now.to_i - Time.parse(token[:created_at]).to_i ) > ( (token[:expires_in] - 60) || 3600 ) )
+      !!token && ( ( Time.now.to_i - Time.parse(token[:created_at]).to_i ) > ( (token[:expires_in].present? ? (token[:expires_in] - 60) : nil) || 3600 ) )
     end
 
     def redis
